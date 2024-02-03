8684a54703dfdf94251b61c3d8642dec0dbd0e21
Steve Chao <22403555+stchao@users.noreply.github.com>
2024-02-03 11:40:05 -0500

    Add serilog and config dependencies

---
+++ Prune/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Prune.Extensions
+{
+    internal static class ServiceCollectionExtension
+    {
+        internal static IServiceCollection ConfigureLogAndServices(this IServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            services
+                .AddSingleton<IConfiguration>(configuration)
+                .AddLogging(configure => configure.AddSerilog());
+
+            return services;
+        }
+    }
+}
--- Prune/Program.cs
+++ Prune/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Prune
+﻿using Microsoft.Extensions.DependencyInjection;
+using Prune.Extensions;
+
+namespace Prune
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var serviceProvider = new ServiceCollection()
+                .ConfigureLogAndServices()
+                .BuildServiceProvider();
+
             Console.WriteLine("Hello, World!");
         }
     }
