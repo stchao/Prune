e0d695f7cedbd13ce29b639879a973f364113004
Steve Chao <22403555+stchao@users.noreply.github.com>
2024-02-04 20:59:28 -0500

    Add logic to get config and start prune

--- Prune/Program.cs
+++ Prune/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Prune.Extensions;
+using Prune.Services;
 
 namespace Prune
 {
@@ -11,7 +14,37 @@ namespace Prune
                 .ConfigureLogAndServices()
                 .BuildServiceProvider();
 
-            Console.WriteLine("Hello, World!");
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+
+            if (logger is null)
+            {
+                Console.WriteLine("Failed to get and/or initialize the logger.");
+                return;
+            }
+
+            try
+            {
+                logger.LogDebug("Getting required service(s).");
+                var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+                var pruneService = serviceProvider.GetRequiredService<IPruneService>();
+                logger.LogInformation("Got required service(s).");
+
+                logger.LogDebug("Getting prune parameter(s).");
+                var pruneParameters = pruneService.GetAndSetPruneConfigurations();
+                logger.LogInformation("Got prune parameter(s).");
+
+                logger.LogDebug("Pruning file(s).");
+                pruneService.PruneDirectories(pruneParameters);
+                logger.LogInformation("Pruned file(s).");
+            }
+            catch (InvalidOperationException ex)
+            {
+                logger.LogError(ex, "Failed to get the required service(s).");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failed to prune file(s).");
+            }
         }
     }
 }
